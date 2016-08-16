75fc2b3917c83aae6ab357d0776d1298f38da71b
Marcel Rodrigues <marcelgmr@gmail.com>
2016-08-16 19:43:08 -0300

    add mbfinfo

---
+++ mbfinfo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "mbf.h"
+
+int
+main(int argc, char *argv[])
+{
+    Font *font;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s font.mbf\n", argv[0]);
+        return 1;
+    }
+    font = load_font(argv[1]);
+    if (font == NULL) {
+        fprintf(stderr, "Failed to load font '%s'.\n", argv[1]);
+        return 1;
+    }
+    printf("chars %u\n", font->header.ng);
+    printf("width %u\n", font->header.w);
+    printf("height %u\n", font->header.h);
+    return 0;
+}
